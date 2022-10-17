0ad61ecb979447192a681d961a90b0731a3bfabc
庄黛淳华 <miku1958@users.noreply.github.com>
2022-10-17 10:52:59 +0800

    Add convince init to Image

---
+++ Sources/Extension/SwiftUI/Image+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Image+Extension.swift
+//  
+//
+//  Created by 庄黛淳华 on 2022/10/15.
+//
+
+import SwiftUI
+import Foundation
+import SFSafeSymbols
+
+extension Image {
+	public static let placeholder = Image(systemSymbol: .photo)
+
+	public init(resourceName: String, in bundle: Bundle = .main) {
+		guard let path = bundle.path(forResource: resourceName, ofType: nil) else {
+#if DEBUG
+			fatalError("file \(resourceName) is missing") 
+#endif
+			self = .placeholder
+			return
+		}
+		self = Self.init(contentsOfFile: path)
+	}
+
+	public init(contentsOfFile path: String) {
+		#if os(macOS)
+		self = NSImage(contentsOfFile: path).map(Image.init(nsImage:)) ?? .placeholder
+		#elseif os(iOS)
+		self = UIImage(contentsOfFile: path).map(Image.init(uiImage:)) ?? .placeholder
+		#else
+		self = .placeholder
+		#endif
+	}
+}
